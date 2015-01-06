a6afb0431eca971a30097b6ce7bddd99b3ee3785
bennyrowland <bennyrowland@mac.com>
2015-01-06 14:54:00 -0500

    changed from using QGraphicsView subclass to QGraphicsScene

--- mrs_gui.py
+++ mrs_gui.py
@@ -3,7 +3,7 @@ __author__ = 'ben'
 from PySide import QtGui, QtCore
 import sys
 
-from pyflow.view import FlowView
+from pyflow.view import FlowScene
 from pyflow.model import FlowModel
 
 if __name__ == "__main__":
@@ -14,7 +14,10 @@ if __name__ == "__main__":
     main_window.resize(400, 400)
     main_window.setWindowTitle("MRS GUI tool v0.1")
 
-    flow_view = FlowView()
+    flow_scene = FlowScene()
+    flow_view = QtGui.QGraphicsView()
+    flow_view.setAlignment(QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop)
+    flow_view.setScene(flow_scene)
     flow_view.setBackgroundBrush(QtCore.Qt.black)
     #flow_view.resize(400, 400)
 
@@ -23,11 +26,13 @@ if __name__ == "__main__":
     flow_model = FlowModel()
     flow_model.add_item()
 
-    flow_view.set_model(flow_model)
+    flow_scene.set_model(flow_model)
 
     flow_model.add_item()
     flow_model.set_data(0, "name", "Hello")
-    flow_model.set_data(0, "position", QtCore.QPoint(50, 50))
+    flow_model.set_data(0, "position", QtCore.QPoint(500, 50))
+
+    flow_view.fitInView(flow_scene.sceneRect(), QtCore.Qt.KeepAspectRatio)
 
     main_window.show()
     sys.exit(my_app.exec_())
